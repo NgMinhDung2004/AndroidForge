08ea8d3baeba25f2b18864cb5dc4c736f79ec1e3
NgMinhDung <dungmap17624@gmail.com>
2026-05-08 23:43:36 +0700

    feat(lab10): add internal and external memory lab

--- Lab10_Internal_and_Axternal_memory/app/src/main/java/com/example/lab10_internal_and_axternal_memory/MainActivity.java
+++ Lab10_Internal_and_Axternal_memory/app/src/main/java/com/example/lab10_internal_and_axternal_memory/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.lab10_internal_and_axternal_memory;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnExercise1 = findViewById(R.id.btnExercise1);
+        Button btnExercise2 = findViewById(R.id.btnExercise2);
+
+        btnExercise1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Exercise1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnExercise2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Exercise2Activity.class);
+                startActivity(intent);
+            }
         });
     }
 }
